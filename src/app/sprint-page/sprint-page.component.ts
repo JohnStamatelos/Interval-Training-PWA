8bec4cb0f05a77b720af8dc5960aaed8169a476a
John Stamatelos <JStamatelos@DVTL101082.local>
2019-08-21 14:23:04 +0200

    sprint page functionality added, repetitions var added to local storage.

--- src/app/sprint-page/sprint-page.component.ts
+++ src/app/sprint-page/sprint-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, timer } from 'rxjs';
+import { take, map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sprint-page',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sprint-page.component.scss']
 })
 export class SprintPageComponent implements OnInit {
+  counter$: Observable<number>;
+  count = 30;
 
-  constructor() { }
+  repetitions: number = +localStorage.getItem('repetitions') + 1;
+
+  constructor(private router: Router) {
+
+    this.counter$ = timer(0, 1000).pipe(
+      take(this.count),
+      map(() => --this.count)
+    );
+    this.startSprintTimer();
+  }
 
   ngOnInit() {
+  }
+
+  startSprintTimer() {
+    setTimeout(() => {
+      this.navigateToSprint();
+    }, (this.count * 1000));
+  }
+
+  navigateToSprint() {
+    if (this.repetitions < 8) {
+      localStorage.setItem('repetitions', this.repetitions.toString())
+      this.router.navigate(['/easy']);
+    } else {
+
+      localStorage.removeItem('repetitions')
+      alert('End of workout!')
+    }
+
   }
 
 }
